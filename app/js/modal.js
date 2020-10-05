6ed9cd2e704ce27dd392bee987938e209bdedee0
JosephDrazen <joe.malovic@gmail.com>
2020-10-05 09:54:36 -0300

    Modal and sidenav fixes
    - added blur effect to modal
    - fixed sidenav bugs
    - changed search button on trading page

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,5 +1,6 @@
 // Get the modal
 let modal = document.getElementById("add-funds-modal");
+let body = document.getElementById("body");
 
 // Get the button that opens the modal
 let btn = document.getElementById("add-funds-link");
@@ -9,16 +10,20 @@ let span = document.getElementsByClassName("close")[0];
 
 btn.addEventListener("click", function() {
   modal.style.display = "block";
+  body.className = "blur";
 });
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  body.style.filter = "none";
+
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    body.className = "";
   }
 }
