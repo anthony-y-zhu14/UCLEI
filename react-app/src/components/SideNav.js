fe0c6dcdceabb91f9375558375e1a5c46e7ba474
JosephDrazen <joe.malovic@gmail.com>
2020-10-27 20:58:38 -0300

    Added navigation

--- react-app/src/components/SideNav.js
+++ react-app/src/components/SideNav.js
@@ -1,9 +1,27 @@
 import React from "react";
 import "./css/SideNav.css";
+import {withRouter} from 'react-router-dom';
 
+let links = {
+  act: '/account',
+  dsh: '/dashboard',
+  trd: '/trading',
+  mrk: '/market'
+};
 
 class Sidenav extends React.Component {
 
+  navToAct = (value) => {
+    this.props.history.push(links.act);
+  }
+
+  navToDsh = (value) => {
+    this.props.history.push(links.dsh);
+  }
+
+  navToTrd = (value) => {
+    this.props.history.push(links.trd);
+  }
 
     render() {
     return(
@@ -11,14 +29,14 @@ class Sidenav extends React.Component {
             <div id="sidenav">
                 <h3 id="sidenav-title">UCLEI</h3>
                 <ul id="sidenav-list">
-                <li>
+                <li onClick={this.navToDsh}>
                     <i className='fa fa-dashboard'></i>
                     Dashboard</li>
-                <li>
+                <li onClick={this.navToAct}>
                     <i className="fa fa-credit-card" aria-hidden="true"></i>
                     Account
                     </li>
-                <li>
+                <li onClick={this.navToTrd}>
                     <i className="fa fa-exchange" aria-hidden="true"></i>
                     Trading</li>
                 <li>
@@ -38,6 +56,4 @@ class Sidenav extends React.Component {
 }
 
 
-
-
-export default Sidenav;
+export default (withRouter)(Sidenav);
