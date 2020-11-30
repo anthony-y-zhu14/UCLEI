4278d3473f1ba03dad61b62d07fe7699b39cbaf8
anthonthon <anthonyzhu1114@gmail.com>
2020-11-30 15:33:36 -0500

    -update

--- server/server/server.js
+++ server/server/server.js
@@ -642,23 +642,55 @@ app.get("/stocks", (req, res) => {
       keys.push(key);
     }
     let symbol = req.query.symbol;
-    if (!symbol){
-        res.write(JSON.stringify(stockDatabase, null, 2));
-        res.end();
-        return;
-    }
+    
     let min = req.query.minprice;
     let max = req.query.maxprice;
 
     let data = [];
     res.setHeader("Content-Type", "application/JSON");
-    if(req.query.symbol != null) {
-      for (let i = 0; i < keys.length; i++) {
-        if(keys[i].includes(symbol)) {
-          data.push(stockDatabase[keys[i]]);
-        }
-      }
-    }    
+    if (!symbol && !min &&!max){
+        res.write(JSON.stringify(stockDatabase, null, 2));
+        res.end();
+        return;
+    }
+
+    // for (let i = 0; i < keys.length; i++) {
+    //     if(keys[i].includes(symbol)) {
+    //       data.push(stockDatabase[keys[i]]);
+    //     }
+    //   }
+
+
+
+
+
+
+    // else if(symbol && !min &&!max) {
+      
+    // }
+    // else if(symbol && min && !max){
+    //     for (const stock in stockDatabase){
+    //         if(stock.includes(symbol) && stockDatabase[stock].quote >= min){
+    //             data.push(stockDatabase[stock]);
+    //         }
+    //     }
+    // }
+    // else if(symbol && !min && max){
+    //     for (const stock in stockDatabase){
+    //         if(stock.includes(symbol) && stockDatabase[stock].quote <= max){
+    //             data.push(stockDatabase[stock]);
+    //         }
+    //     }
+    // }
+    // else if(symbol && min && max){
+    //     for (const stock in stockDatabase){
+    //         if(stock.includes(symbol) && stockDatabase[stock].quote <= max){
+    //             data.push(stockDatabase[stock]);
+    //         }
+    //     }
+    // }
+    
+
 
     res.write(JSON.stringify(data, null, 2));
     res.end();
